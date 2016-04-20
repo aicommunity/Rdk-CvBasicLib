7b0bf39f7a1253ca940034d2a97bc15aad93a70e
APopova <devnull@localhost>
2016-04-20 15:57:23 +0300

    Добавлены mk файлы.

---
+++ Build/Eclipse/Rdk-CvBasicLib/Debug/Core/subdir.mk
@@ -0,0 +1,144 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/Basic.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAColorConvert.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBACrop.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAFlipImage.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAModel.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAReceiver.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAReduce.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAResize.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBARotate.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBASource.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBASourceFile.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBASourceMultiFile.cpp \
+/home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBPipeline.cpp 
+
+OBJS += \
+./Core/Basic.o \
+./Core/UBAColorConvert.o \
+./Core/UBACrop.o \
+./Core/UBAFlipImage.o \
+./Core/UBAModel.o \
+./Core/UBAReceiver.o \
+./Core/UBAReduce.o \
+./Core/UBAResize.o \
+./Core/UBARotate.o \
+./Core/UBASource.o \
+./Core/UBASourceFile.o \
+./Core/UBASourceMultiFile.o \
+./Core/UBPipeline.o 
+
+CPP_DEPS += \
+./Core/Basic.d \
+./Core/UBAColorConvert.d \
+./Core/UBACrop.d \
+./Core/UBAFlipImage.d \
+./Core/UBAModel.d \
+./Core/UBAReceiver.d \
+./Core/UBAReduce.d \
+./Core/UBAResize.d \
+./Core/UBARotate.d \
+./Core/UBASource.d \
+./Core/UBASourceFile.d \
+./Core/UBASourceMultiFile.d \
+./Core/UBPipeline.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Basic.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/Basic.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBAColorConvert.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAColorConvert.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBACrop.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBACrop.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBAFlipImage.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAFlipImage.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBAModel.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAModel.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBAReceiver.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAReceiver.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBAReduce.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAReduce.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBAResize.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBAResize.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBARotate.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBARotate.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBASource.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBASource.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBASourceFile.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBASourceFile.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBASourceMultiFile.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBASourceMultiFile.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Core/UBPipeline.o: /home/klavesin/workspace/tops-klavesin/Klavesin-STZ/Libraries/Rdk-CvBasicLib/Core/UBPipeline.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
